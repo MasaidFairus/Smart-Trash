eeb34a225f6f91c322226f4bfa6f1a39e793d5c7
Masaid Fairus Trimarsongko <106062448+MasaidFairus@users.noreply.github.com>
2023-02-06 14:00:57 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+let distance = 0
+basic.forever(function () {
+    distance = sonar.ping(
+    DigitalPin.P3,
+    DigitalPin.P4,
+    PingUnit.Centimeters
+    )
+    whaleysans.showNumber(distance)
+    if (distance > 0 && distance < 20) {
+        pins.servoWritePin(AnalogPin.P0, 90)
+        basic.pause(1000)
+    } else {
+        pins.servoWritePin(AnalogPin.P0, 0)
+        basic.pause(500)
+    }
+    basic.pause(100)
+})
